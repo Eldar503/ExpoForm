14b54c72812623035400557067454ecc4898e2b9
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-06 23:26:34 +0300

    ААА

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,7 @@
-html[lang="ru"] [lang="en"] {
-    display: none;
-  }
-  html[lang="en"] [lang="ru"] {
-    display: none;
-  }
-  
-  <ul class="langswitcher">
-  <li class="active"><a href="#ru" data-lang="ru">Ru</a></li>
-  <li><a href="#en" data-lang="en">En</a></li>
-  </ul>
-  
-  $(".langswitcher a").on("click",function(e){
+
+
+
+$(".langswitcher a").on("click",function(e){
     e.preventDefault();
     $(".langswitcher li").removeClass("active");
     $("html").attr("lang",$(this).data("lang"));
