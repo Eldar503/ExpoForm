06739ff15625dc84c3ff2be4f89b7e8edda7d472
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-07 21:57:58 +0300

    Удаления js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,2 @@
 
 
-
-$(".langswitcher a").on("click",function(e){
-    e.preventDefault();
-    $(".langswitcher li").removeClass("active");
-    $("html").attr("lang",$(this).data("lang"));
-    $(this).parent().addClass("active");
-  });
